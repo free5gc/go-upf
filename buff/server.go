077b2a1e82db78bef97fe792b1d4144d6e097cfe
HIRONO Koji <koji.hirono.yh@apresiasystems.co.jp>
2021-09-27 16:36:23 +0900

    Add buff

---
+++ buff/server.go
@@ -0,0 +1,104 @@
+package forwarder
+
+import (
+	"net"
+	"os"
+	"unsafe"
+)
+
+const (
+	BUFF = 1 << 2
+	NOCP = 1 << 3
+)
+
+type Server struct {
+	conn   *net.UnixConn
+	f      *os.File
+	q      map[uint16]chan []byte
+	qlen   int
+	notify func(uint16)
+}
+
+func OpenServer(addr string, qlen int, notify func(uint16)) (*Server, error) {
+	s := new(Server)
+
+	laddr, err := net.ResolveUnixAddr("unixgram", addr)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.ListenUnixgram("unixgram", laddr)
+	if err != nil {
+		return nil, err
+	}
+	s.conn = conn
+
+	f, err := conn.File()
+	if err != nil {
+		s.Close()
+		return nil, err
+	}
+	s.f = f
+
+	s.q = make(map[uint16]chan []byte)
+	s.qlen = qlen
+
+	s.notify = notify
+
+	go s.Serve()
+
+	return s, nil
+}
+
+func (s *Server) Close() {
+	if s.conn != nil {
+		s.conn.Close()
+	}
+	if s.f != nil {
+		s.f.Close()
+	}
+}
+
+func (s *Server) Serve() {
+	b := make([]byte, 96*1024)
+	for {
+		n, _, err := s.conn.ReadFrom(b)
+		if err != nil {
+			break
+		}
+		if n < 4 {
+			continue
+		}
+		pdrid := *(*uint16)(unsafe.Pointer(&b[0]))
+		action := *(*uint16)(unsafe.Pointer(&b[2]))
+		if action&BUFF == 0 {
+			continue
+		}
+		if action&NOCP != 0 {
+			s.notify(pdrid)
+		}
+		pkt := make([]byte, n-4)
+		copy(pkt, b[4:n])
+		q, ok := s.q[pdrid]
+		if !ok {
+			s.q[pdrid] = make(chan []byte, s.qlen)
+			q = s.q[pdrid]
+		}
+		q <- pkt
+	}
+	for _, q := range s.q {
+		close(q)
+	}
+}
+
+func (s *Server) Pop(pdrid uint16) ([]byte, bool) {
+	q, ok := s.q[pdrid]
+	if !ok {
+		return nil, ok
+	}
+	select {
+	case pkt := <-q:
+		return pkt, true
+	default:
+		return nil, false
+	}
+}
---
+++ buff/server_test.go
@@ -0,0 +1,67 @@
+package forwarder
+
+import (
+	"bytes"
+	"net"
+	"os"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	done := make(chan uint16)
+	defer close(done)
+	addr := "test.unsock"
+	qlen := 10
+	os.Remove(addr)
+	s, err := OpenServer(addr, qlen, func(pdrid uint16) {
+		done <- pdrid
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+	defer os.Remove(addr)
+
+	laddr, err := net.ResolveUnixAddr("unixgram", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn, err := net.DialUnix("unixgram", nil, laddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	pkt := []byte{
+		0x03, 0x00,
+		0x0c, 0x00,
+		0xee, 0xbb,
+		0xdd, 0xcc,
+	}
+
+	_, err = conn.Write(pkt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pdrid := <-done
+
+	if pdrid != 3 {
+		t.Errorf("want %v; but got %v\n", 3, pdrid)
+	}
+
+	pkt, ok := s.Pop(pdrid)
+	if !ok {
+		t.Fatal("not found")
+	}
+
+	want := []byte{0xee, 0xbb, 0xdd, 0xcc}
+	if !bytes.Equal(pkt, want) {
+		t.Errorf("want %x; but got %x\n", want, pkt)
+	}
+
+	_, ok = s.Pop(pdrid)
+	if ok {
+		t.Fatal("found")
+	}
+}
