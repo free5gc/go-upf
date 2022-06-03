25b0963e30ab59fe91d3e8da1c06952d593df880
free5gc-org <free5gc.org@gmail.com>
2022-06-03 16:24:19 +0000

    add USAReport to Report interface and ServeUSAReport handler

--- internal/pfcp/report.go
+++ internal/pfcp/report.go
@@ -21,26 +21,32 @@ func (s *PfcpServer) ServeReport(r *report.SessReport) {
 		return
 	}
 
+	addr := fmt.Sprintf("%s:%d", sess.rnode.ID, factory.UpfPfcpDefaultPort)
+	laddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return
+	}
+
 	if r.Action&buff.BUFF != 0 && len(r.BufPkt) > 0 {
 		dldr, ok := r.Report.(report.DLDReport)
 		if ok {
 			sess.Push(dldr.PDRID, r.BufPkt)
 		}
 	}
-	if r.Action&buff.NOCP != 0 {
-		addr := fmt.Sprintf("%s:%d", sess.rnode.ID, factory.UpfPfcpDefaultPort)
-		laddr, err := net.ResolveUDPAddr("udp", addr)
+	if r.Action&buff.NOCP != 0 && r.Report.Type() == report.DLDR {
+		dldr := r.Report.(report.DLDReport)
+		err := s.ServeDLDReport(laddr, r.SEID, dldr.PDRID)
 		if err != nil {
-			return
+			s.log.Errorln(err)
 		}
+	}
 
-		switch r.Report.Type() {
-		case report.DLDR:
-			dldr := r.Report.(report.DLDReport)
-			err := s.ServeDLDReport(laddr, r.SEID, dldr.PDRID)
-			if err != nil {
-				s.log.Errorln(err)
-			}
+	switch r.Report.Type() {
+	case report.USAR:
+		usar := r.Report.(report.USAReport)
+		err := s.ServeUSAReport(laddr, r.SEID, &usar)
+		if err != nil {
+			s.log.Errorln(err)
 		}
 	}
 }
@@ -70,4 +76,31 @@ func (s *PfcpServer) ServeDLDReport(addr net.Addr, seid uint64, pdrid uint16) er
 
 	err := s.sendReqTo(req, addr, nil) // No waiting for rsp
 	return errors.Wrap(err, "ServeDLDReport")
+}
+
+func (s *PfcpServer) ServeUSAReport(addr net.Addr, seid uint64, usar *report.USAReport) error {
+	s.log.Infoln("ServeUSAReport")
+
+	tr := &usar.USARTrigger
+	req := message.NewSessionReportRequest(
+		0,
+		0,
+		seid,
+		0,
+		0,
+		ie.NewReportType(0, 0, 1, 0),
+		ie.NewUsageReportWithinSessionReportRequest(
+			ie.NewURRID(usar.URRID),
+			ie.NewURSEQN(usar.URSEQN),
+			ie.NewUsageReportTrigger(
+				tr.PERIO|tr.VOLTH<<1|tr.TIMTH<<2|tr.QUHTI<<3|tr.START<<4|tr.STOPT<<5|tr.DROTH<<6|tr.IMMER<<7,
+				tr.VOLQU|tr.TIMQU<<1|tr.LIUSA<<2|tr.TERMR<<3|tr.MONIT<<4|tr.ENVCL<<5|tr.MACAR<<6|tr.EVETH<<7,
+				tr.EVEQU|tr.TEBUR<<1|tr.IPMJL<<2|tr.QUVTI<<3|tr.EMRRE<<4,
+			),
+			// TODO:
+		),
+	)
+
+	err := s.sendReqTo(req, addr, nil) // No waiting for rsp
+	return errors.Wrap(err, "ServeUSAReport")
 }
--- internal/report/report.go
+++ internal/report/report.go
@@ -22,6 +22,40 @@ func (r DLDReport) Type() int {
 	return DLDR
 }
 
+type USAReport struct {
+	URRID       uint32
+	URSEQN      uint32
+	USARTrigger UsageReportTrigger
+}
+
+type UsageReportTrigger struct {
+	PERIO uint8
+	VOLTH uint8
+	TIMTH uint8
+	QUHTI uint8
+	START uint8
+	STOPT uint8
+	DROTH uint8
+	IMMER uint8
+	VOLQU uint8
+	TIMQU uint8
+	LIUSA uint8
+	TERMR uint8
+	MONIT uint8
+	ENVCL uint8
+	MACAR uint8
+	EVETH uint8
+	EVEQU uint8
+	TEBUR uint8
+	IPMJL uint8
+	QUVTI uint8
+	EMRRE uint8
+}
+
+func (r USAReport) Type() int {
+	return USAR
+}
+
 type SessReport struct {
 	SEID   uint64
 	Report Report
