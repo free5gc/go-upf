71fabdabdacbdb42da85e0d59947c8adc6ab9742
ianchen0119 <ychen.desl@gmail.com>
2022-07-05 15:45:49 +0000

    update error msg in handleSessionModificationRequest()

--- internal/pfcp/session.go
+++ internal/pfcp/session.go
@@ -152,105 +152,105 @@ func (s *PfcpServer) handleSessionModificationRequest(
 	for _, i := range req.CreateFAR {
 		err = sess.CreateFAR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("CreateFAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.CreateQER {
 		err = sess.CreateQER(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("CreateQER error: %+v", err)
 		}
 	}
 
 	for _, i := range req.CreateURR {
 		err = sess.CreateURR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("CreateURR error: %+v", err)
 		}
 	}
 
 	if req.CreateBAR != nil {
 		err = sess.CreateBAR(req.CreateBAR)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("CreateBAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.CreatePDR {
 		err = sess.CreatePDR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("CreatePDR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.RemoveFAR {
 		err = sess.RemoveFAR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("RemoveFAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.RemoveQER {
 		err = sess.RemoveQER(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("RemoveQER error: %+v", err)
 		}
 	}
 
 	for _, i := range req.RemoveURR {
 		err = sess.RemoveURR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("RemoveURR error: %+v", err)
 		}
 	}
 
 	if req.RemoveBAR != nil {
 		err = sess.RemoveBAR(req.RemoveBAR)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("RemoveBAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.RemovePDR {
 		err = sess.RemovePDR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("RemovePDR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.UpdateFAR {
 		err = sess.UpdateFAR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("UpdateFAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.UpdateQER {
 		err = sess.UpdateQER(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("UpdateQER error: %+v", err)
 		}
 	}
 
 	for _, i := range req.UpdateURR {
 		err = sess.UpdateURR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("UpdateURR error: %+v", err)
 		}
 	}
 
 	if req.UpdateBAR != nil {
 		err = sess.UpdateBAR(req.UpdateBAR)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("UpdateBAR error: %+v", err)
 		}
 	}
 
 	for _, i := range req.UpdatePDR {
 		err = sess.UpdatePDR(i)
 		if err != nil {
-			sess.log.Errorln(err)
+			sess.log.Errorf("UpdatePDR error: %+v", err)
 		}
 	}
 
