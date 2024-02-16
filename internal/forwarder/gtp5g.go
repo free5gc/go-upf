52fdc334cd68c6f03004ff3a983f3cd926b1a20c
BrianChen <46628572+brianchennn@users.noreply.github.com>
2024-02-16 18:50:44 +0800

    fix: flow description should accept token "assigned" (#41)
    * fix: flow description should accept token "assigned"
    
    * fix: swap srt, dst IP/Port in when newPdi
    
    * swapSrcDst() in newSdfFilter()
    
    * fix to handle multi sdfIE and refactor
    
    ---------
    
    Co-authored-by: Tim Liu <timliu@nctu.edu.tw>

--- internal/forwarder/gtp5g.go
+++ internal/forwarder/gtp5g.go
@@ -173,11 +173,14 @@ func (g *Gtp5g) Link() *Gtp5gLink {
 	return g.link
 }
 
-func (g *Gtp5g) newFlowDesc(s string) (nl.AttrList, error) {
+func (g *Gtp5g) newFlowDesc(s string, swapSrcDst bool) (nl.AttrList, error) {
 	var attrs nl.AttrList
 	fd, err := ParseFlowDesc(s)
 	if err != nil {
 		return nil, err
+	}
+	if swapSrcDst {
+		fd.Src, fd.Dst = fd.Dst, fd.Src
 	}
 	switch fd.Action {
 	case "permit":
@@ -249,7 +252,7 @@ func convertSlice(ports [][]uint16) []byte {
 	return b
 }
 
-func (g *Gtp5g) newSdfFilter(i *ie.IE) (nl.AttrList, error) {
+func (g *Gtp5g) newSdfFilter(i *ie.IE, srcIf uint8) (nl.AttrList, error) {
 	var attrs nl.AttrList
 
 	v, err := i.SDFFilter()
@@ -258,7 +261,8 @@ func (g *Gtp5g) newSdfFilter(i *ie.IE) (nl.AttrList, error) {
 	}
 
 	if v.HasFD() {
-		fd, err := g.newFlowDesc(v.FlowDescription)
+		swapSrcDst := (srcIf == ie.SrcInterfaceAccess)
+		fd, err := g.newFlowDesc(v.FlowDescription, swapSrcDst)
 		if err != nil {
 			return nil, err
 		}
@@ -311,9 +315,17 @@ func (g *Gtp5g) newPdi(i *ie.IE) (nl.AttrList, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	var srcIf uint8
+	var sdfIEs []*ie.IE
 	for _, x := range ies {
 		switch x.Type {
 		case ie.SourceInterface:
+			v, err := x.SourceInterface()
+			if err != nil {
+				break
+			}
+			srcIf = v
 		case ie.FTEID:
 			v, err := x.FTEID()
 			if err != nil {
@@ -343,15 +355,18 @@ func (g *Gtp5g) newPdi(i *ie.IE) (nl.AttrList, error) {
 				Value: nl.AttrBytes(v.IPv4Address),
 			})
 		case ie.SDFFilter:
-			v, err := g.newSdfFilter(x)
-			if err != nil {
-				break
-			}
+			sdfIEs = append(sdfIEs, x)
+		case ie.ApplicationID:
+		}
+	}
+
+	for _, x := range sdfIEs {
+		v, err := g.newSdfFilter(x, srcIf)
+		if err == nil {
 			attrs = append(attrs, nl.Attr{
 				Type:  gtp5gnl.PDI_SDF_FILTER,
 				Value: v,
 			})
-		case ie.ApplicationID:
 		}
 	}
 
